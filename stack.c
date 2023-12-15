18c278c943e3769eb7514e60bb78047e99e330ea
joefas <ochienggeofrey06@gmail.com>
2023-12-15 08:25:10 -0800

    monty file

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+/**
+ * f_stack - prints the top
+ * @counter: line number
+ * @head: stack head
+ * Return: 0 always
+ */
+void f_stack(stack_t **head, unsigned int counter)
+{
+	counter(void);
+	head(void);
+
+	tap.lifi = 0;
+}
